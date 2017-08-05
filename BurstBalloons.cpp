3a36f5bfc9c0358ff8cce9b4da954c20233c0f6b
mm10086 <30691818+mm10086@users.noreply.github.com>
2017-08-05 17:06:20 +0800

    Update BurstBalloons.cpp

--- BurstBalloons.cpp
+++ BurstBalloons.cpp
@@ -1,7 +1,8 @@
 /*
 1、先定义状态dp[i][j]表示打爆区间[i,j]中的所有气球能得到的最多金币
 2、找状态转移方程dp[i][j] = max(dp[i][j], nums[i - 1] * nums[k] * nums[j + 1] + dp[i][k - 1] + dp[k + 1][j]) (i ≤ k ≤ j)
-
+    k代表区间[i, j]中最后打爆的气球
+3、设计递推，求解dp[i][j]
 */
 class Solution {
 public:
@@ -10,6 +11,7 @@ public:
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
         vector<vector<int>> dp(nums.size(), vector<int>(nums.size() , 0));
+        //外层循环采用区间[i, j]长度len递增的形式，保证递推式始终成立
         for (int len = 1; len <= n; ++len) {
             for (int left = 1; left <= n - len + 1; ++left) {
                 int right = left + len - 1;
